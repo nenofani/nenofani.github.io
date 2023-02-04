1e96340e3b84d17c07eafb1fca243bcd77ba88c9
nenofani <nefasa05@gmail.com>
2023-02-04 16:35:09 +0700

    portfolio tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["index.html"],
+  theme: {
+    container: {
+      center: true,
+      padding: "16px",
+    },
+    extend: {
+      fontFamily: {
+        inter: ["Inter"],
+        poppins: ["Poppins"],
+        roboto: ["Roboto"],
+      },
+      colors: {
+        primary: "#f43f5e",
+        secondary: "#64748b",
+        dark: "#0f172a",
+      },
+      screens: {
+        "2xl": "1320px",
+        hp: { max: "640px" },
+      },
+    },
+  },
+  plugins: [],
+};
